d3ebabe9148b98a795b6f1961e183405f4120c8c
Piotr JANKOWSKI <pjankowski@sii.pl>
2023-06-20 12:50:18 +0200

    added security

---
+++ src/main/java/pl/sii/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package pl.sii.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        auth -> auth.anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .build();
+    }
+}
